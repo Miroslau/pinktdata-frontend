1f1a85cceb8d8edcf039e578735bcca8c1502f2c
Dziana Kavaleva <dianakove32@gmail.com>
2021-10-13 22:44:31 +0300

    add icons to footer

--- src/components/section-components/footer/Footer.js
+++ src/components/section-components/footer/Footer.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Footer.scss';
+import instagram from '../../../assets/instagram 1.svg';
+import youtub from '../../../assets/youtub.svg';
+import twitter from '../../../assets/twitter.svg';
+import facebook from '../../../assets/icons8-facebook-новый.svg';
+
+const icons = [instagram, youtub, twitter, facebook];
 
 export default function Footer() {
   return (
@@ -14,7 +20,16 @@ export default function Footer() {
             specimen book. It has survived not only five centuries, but also the leap into
             electronic typesetting
           </p>
-          <div className="icons" />
+          <div className="icons">
+            {icons.map((icon) => (
+              <object
+                aria-label="this object has text"
+                className="object"
+                type="image/svg+xml"
+                data={icon}
+              />
+            ))}
+          </div>
         </div>
         <div className="footer-nav">
           <Link to="/">Home</Link>
