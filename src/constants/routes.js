d9ba30c1092fa11202dbb921a8bd060b2358dcf2
Finsoy <vadimka.cry.00@gmail.com>
2021-10-20 14:48:47 +0300

    fix: naming routes

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,6 +1,6 @@
 export const LANDING_ROUTE = '/';
-export const MAP_ROUTE = '/map_page';
+export const MAP_ROUTE = '/map';
 export const BUY_PAGE = '/buy';
-export const SALE_PAGE = '/forSale';
+export const SALE_PAGE = '/sale';
 export const INSIGHT_PAGE = '/insight';
-export const ROOM_PAGE = '/apartments/getApartment/:id';
+export const ROOM_PAGE = '/apartments/:id';
