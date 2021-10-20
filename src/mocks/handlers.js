9130ab1765200ee063869a240342cb3b31dd5b7b
Miroslau <mira2408@mail.ru>
2021-10-20 13:42:20 +0300

    feat: add mocko data in major city

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -35,6 +35,33 @@ export const handlers = [
       accessToken: 'Bear f79e82e8-c34a-4dc7-a49e-9fadc0979fda',
     }));
   }),
+  rest.get('http://localhost:3000/api/cities', (req, res, ctx) => res(ctx.json([
+    {
+      id: 1,
+      title: 'Los Angeles',
+      img: 'https://i.pinimg.com/564x/06/77/a7/0677a7a87420d91536f200d921694fb8.jpg',
+    },
+    {
+      id: 2,
+      title: 'Miami',
+      img: 'https://i.pinimg.com/564x/88/bb/cb/88bbcbf3579b880d81f7dcc969c89e44.jpg',
+    },
+    {
+      id: 3,
+      title: 'San Francisco',
+      img: 'https://i.pinimg.com/564x/23/84/66/238466913b4fdeb5eec4cd44b57307f2.jpg',
+    },
+    {
+      id: 4,
+      title: 'Houston',
+      img: 'https://i.pinimg.com/564x/14/7e/1d/147e1df9f7619e9e5d14398aaa7030ee.jpg',
+    },
+    {
+      id: 5,
+      title: 'New York',
+      img: 'https://i.pinimg.com/564x/cc/30/74/cc30746029f69d8de4f88153e243888d.jpg',
+    },
+  ]))),
   rest.post('http://localhost:3000/api/auth/logout', (req, res, ctx) => res(ctx.status(204))),
   rest.get('/api/search/location', (req, res, ctx) => res(ctx.status(200), ctx.json({
     title: 'test',
