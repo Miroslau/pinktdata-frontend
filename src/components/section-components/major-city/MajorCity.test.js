f37687e40de9c23b7b57e1746192f9e87fcec9ef
Miroslau <mira2408@mail.ru>
2021-10-21 12:45:52 +0300

    fix: fix test for major cities

--- src/components/section-components/major-city/MajorCity.test.js
+++ src/components/section-components/major-city/MajorCity.test.js
@@ -1,11 +1,45 @@
 import '@testing-library/jest-dom';
 import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { configureStore, createSlice, combineReducers } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer } from 'redux-persist';
+import { BrowserRouter } from 'react-router-dom';
 import MajorCity from './MajorCity';
 import { server } from '../../../mocks/server';
 
 // eslint-disable-next-line no-undef
 describe('MajorCity Component', () => {
   process.env.REACT_APP_IS_MOCKING = true;
+
+  const majorCitySlice = createSlice({
+    name: 'majorCity',
+    initialState: {
+      city: '',
+      publicAddress: '',
+      imageUrl: '',
+      count: 0,
+    },
+  });
+
+  const majorCityReducer = majorCitySlice.reducer;
+
+  const persistConfig = {
+    key: 'root',
+    storage,
+  };
+
+  const persistedReducer = persistReducer(persistConfig, majorCityReducer);
+
+  const reducers = combineReducers({
+    majorCity: persistedReducer,
+  });
+
+  const rootReducer = (state, action) => reducers(state, action);
+
+  const store = configureStore({
+    reducer: rootReducer,
+  });
 
   // eslint-disable-next-line no-undef
   beforeAll(() => server.listen({
@@ -19,13 +53,13 @@ describe('MajorCity Component', () => {
   // eslint-disable-next-line no-undef
   it('renders component Major cities', () => {
     // eslint-disable-next-line react/react-in-jsx-scope
-    render(<MajorCity />);
+    render(<Provider store={store}><BrowserRouter><MajorCity /></BrowserRouter></Provider>);
   });
 
   // eslint-disable-next-line no-undef
   it('fetches major cities from an API', async () => {
-    // eslint-disable-next-line react/react-in-jsx-scope
-    const { findByText } = render(<MajorCity />);
+    // eslint-disable-next-line react/react-in-jsx-scope,max-len
+    const { findByText } = render(<Provider store={store}><BrowserRouter><MajorCity /></BrowserRouter></Provider>);
     // eslint-disable-next-line no-undef
     expect(await findByText('Los Angeles')).toBeInTheDocument();
   });
