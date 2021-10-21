db0b8058a73715fedb42709a4c08c070dca66850
Dziana Kavaleva <dianakove32@gmail.com>
2021-10-21 12:55:35 +0300

    fix pull mistks

---
+++ src/components/section-components/main-screen/main-search/MainSearch.test.js
@@ -0,0 +1,10 @@
+import getCities from './MainSearch';
+
+global.fetch = jest.fn(() => Promise.resolve({
+  json: () => Promise.resolve({ cities: ['minsk', 'gomel', 'mogileu', 'moscow', 'miami'] }),
+}));
+it('searchLocation shoud return cities', () => {
+  const data = getCities('m');
+  expect(data).toBe(['minsk', 'gomel', 'mogileu', 'moscow', 'miami']);
+  expect(fetch).toHaveBeenCalledTimes(1);
+});
