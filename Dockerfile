2133bb40ac6e96452ce85b2bc94811ce9116b0d8
Catherine <60444425+MonkeyKingBar-bit@users.noreply.github.com>
2021-10-25 14:05:17 +0300

    Revert "Revert 18 feature/pin 29"

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM node:14.18.0
-RUN mkdir -p /app
+# RUN mkdir -p /app
 WORKDIR /app
 COPY package*.json /app
 RUN npm install
--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-
-import { Route, Switch } from 'react-router';
-import './App.css';
-import MainPage from './components/main-page/MainPage';
-import Buy from './components/Pages/Buy/Buy';
-import ForSale from './components/Pages/ForSale/ForSale';
-import Insight from './components/Pages/Insight/Insight';
+import { BrowserRouter } from 'react-router-dom';
+import React from 'react';
+import AppRouter from './components/app-router/AppRouter';
 
 function App() {
   return (
-    <div className="App">
-      <Switch>
-      <Route exact path="/" render={(props) => <MainPage {...props} />} />
-
-        <Route exact path="/buy" render={(props) => <Buy {...props} />} />
-        <Route path="/forSale" render={(props) => <ForSale {...props} />} />
-        <Route path="/insight" render={(props) => <Insight {...props} />} />
-        {/* <Route path = "/contact" render = {(props) => <Contact   {...props} />} /> */}
-        {/* <Route path = '*' component = {NotFoundPage} /> */}
-
-      </Switch>
-    </div>
+    <BrowserRouter>
+      <AppRouter />
+    </BrowserRouter>
   );
 }
 
