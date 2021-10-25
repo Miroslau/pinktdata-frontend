35acfab206f11380017153825623239da6b0ab17
Miroslau <mira2408@mail.ru>
2021-10-25 16:59:09 +0300

    fix: add map page

---
+++ src/components/map-page/card/Card.jsx
@@ -0,0 +1,72 @@
+import React from 'react';
+
+import Divider from '@mui/material/Divider';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import PropTypes from 'prop-types';
+import StarIcon from '@mui/icons-material/Star';
+// import TypographyMui from '../../ui-components/typography-mui/TypographyMui';
+import useStyles from '../../../style/style';
+
+const Card = (props) => {
+  const classes = useStyles();
+
+  const {
+    name, rating, img, reviews, price,
+  } = props;
+
+  return (
+    <>
+      <div className={classes.mapContent}>
+        <div className={classes.content}>
+          <div className={classes.contentImg}>
+            <img className={classes.img} src={img} alt={name} />
+          </div>
+          <div className={classes.contentData}>
+            <div className={classes.dataLeft}>
+              <div className={classes.dataText}>
+                {/* <TypographyMui text={city} className={classes.city} /> */}
+                {/* <TypographyMui variant="h5" className={classes.name} text={name} /> */}
+                {/* eslint-disable-next-line max-len */}
+                {/* <TypographyMui color="#32CD32" text={DATA[0]} className={classes.available} /> */}
+                {/* eslint-disable-next-line max-len */}
+                {/* <TypographyMui text={`${homeDetails}.${address}`} className={classes.address} /> */}
+              </div>
+              <div className={classes.rating}>
+                <StarIcon className={classes.starIcon} />
+                {rating}
+                <div className={classes.reviews}>
+                  (
+                  {reviews}
+                  )
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={classes.dataRight}>
+            <div className={classes.heart}><FavoriteBorderIcon htmlColor="pink" /></div>
+            <div className={classes.price}>{price}</div>
+          </div>
+        </div>
+      </div>
+      <Divider />
+    </>
+  );
+};
+
+Card.defaultProps = {
+  rating: '',
+  name: '',
+  img: '',
+  reviews: '',
+  price: '',
+};
+
+Card.propTypes = {
+  name: PropTypes.string,
+  img: PropTypes.string,
+  rating: PropTypes.number,
+  reviews: PropTypes.number,
+  price: PropTypes.string,
+};
+
+export default Card;
