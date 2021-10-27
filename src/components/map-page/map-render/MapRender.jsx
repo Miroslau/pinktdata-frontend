41967af0f561a5f1e81bfbcf89d5b09d133767d8
Miroslau <mira2408@mail.ru>
2021-10-27 15:47:17 +0300

    fix: fix a map

--- src/components/map-page/map-render/MapRender.jsx
+++ src/components/map-page/map-render/MapRender.jsx
@@ -1,16 +1,33 @@
 import {
   useEffect, useState,
 } from 'react';
+import L from 'leaflet';
 import {
-  MapContainer, TileLayer, CircleMarker, Popup, Tooltip, useMap,
+  MapContainer, TileLayer, Popup, Tooltip, useMap, Marker,
 } from 'react-leaflet';
+import MarkerClusterGroup from 'react-leaflet-markercluster/src/react-leaflet-markercluster';
 import { useSelector } from 'react-redux';
 import { apartmentSelector } from '../../../store/slice/apartmentSlice';
 import useMountedState from '../../../hooks/useMountedState';
+import pointMarker from '../../../assets/svg/pointMarker.svg';
 
 import MapAPI from '../../../api/map/mapPageAPI';
 
 import useStyles from '../../../style/style';
+
+const markerIcon = new L.Icon({
+  iconUrl: pointMarker,
+  iconSize: new L.Point(32, 32),
+  shadowUrl: null,
+  shadowSize: null,
+  shadowAnchor: null,
+});
+
+const createClusterCustomIcon = (cluster) => L.divIcon({
+  html: `<span>${cluster.getChildCount()}</span>`,
+  className: 'marker-cluster-custom',
+  iconSize: L.point(40, 40, true),
+});
 
 const MapRender = () => {
   const classes = useStyles();
@@ -52,30 +69,30 @@ const MapRender = () => {
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        {apart.map((data) => (
-          <CircleMarker
-            key={data._id}
-            center={[data.location.lat, data.location.lon]}
-            pathOptions={{
-              color: 'blue',
-              fillColor: ' blue',
-              weight: 1,
-              opacity: 1,
-              fillOpacity: 0.2,
-            }}
-            radius={10}
-          >
-            <Tooltip permanent>{data.price}</Tooltip>
-            <Popup>
-              {data.address}
-              {' '}
-              <br />
-              {' '}
-              {data.price}
-            </Popup>
-          </CircleMarker>
-        ))}
-        <SetViewOnFetch coords={location} />
+        <MarkerClusterGroup
+          iconCreateFunction={createClusterCustomIcon}
+          showCoverageOnHover={false}
+          spiderLegPolylineOptions={{ opacity: 0 }}
+        >
+          {apart.map((data) => (
+            <Marker
+              key={data._id}
+              center={[data.location.lat, data.location.lon]}
+              position={[data.location.lat, data.location.lon]}
+              icon={markerIcon}
+            >
+              <Tooltip direction="top" offset={[0, -5]} permanent>{data.price}</Tooltip>
+              <Popup>
+                {data.address}
+                {' '}
+                <br />
+                {' '}
+                {data.price}
+              </Popup>
+            </Marker>
+          ))}
+          <SetViewOnFetch coords={location} />
+        </MarkerClusterGroup>
       </MapContainer>
     </div>
   );
