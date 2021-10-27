a305ec1ebfa34f3232957db6d01cde9a6789dbf3
Finsoy <vadimka.cry.00@gmail.com>
2021-10-27 19:47:09 +0300

    feat: add labels for stars in PreviewPage

---
+++ src/constants/ratingLabels.js
@@ -0,0 +1,13 @@
+export const ratingLabels = {
+  0.5: 'Useless',
+  1: 'Useless+',
+  1.5: 'Poor',
+  2: 'Poor+',
+  2.5: 'Ok',
+  3: 'Ok+',
+  3.5: 'Good',
+  4: 'Good+',
+  4.5: 'Excellent',
+  5: 'Excellent+',
+
+};
