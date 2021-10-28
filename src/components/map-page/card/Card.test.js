eb1e100e87e399b81a4c7b641cdbafbbeba10ef1
Raman Novikau <demoikk@gmail.com>
2021-10-28 15:35:10 +0300

    fix: change image slider tests

--- src/components/map-page/card/Card.test.js
+++ src/components/map-page/card/Card.test.js
@@ -1,46 +1,49 @@
+/* eslint-disable react/jsx-one-expression-per-line */
+/* eslint-disable max-len */
 import '@testing-library/jest-dom';
 import {
-  render,
+  render, fireEvent,
 } from '@testing-library/react';
 import { Splide, SplideSlide } from '@splidejs/react-splide';
-import Card from './Card';
 import apartments from '../../../mocks/mocks-constants/mockDataApartments';
 
 describe('Card component', () => {
-  let unmount;
   const mockData = apartments[0];
 
-  beforeEach(() => {
-    const { unmount: currentUnmount } = render(<Card
-      id={mockData._id}
-      name={mockData.name}
-      img={mockData.img}
-      rating={mockData.rating}
-      reviews={mockData.reviews}
-      city={mockData.city}
-      address={mockData.address}
-      price={mockData.price}
-      homeDetails={mockData.guestLabel}
-      images={mockData.images}
-    />);
-    unmount = currentUnmount;
-  });
+  describe('image slider', () => {
+    let container;
+    let unmount;
 
-  afterEach(() => {
-    unmount();
-  });
+    beforeEach(() => {
+      const options = {
+        perPage: 1,
+        perMove: 1,
+        rewind: true,
+        fixedWidth: '350px',
+        fixedHeight: '250px',
+        cover: 'true',
+        pagination: true,
+      };
+      const component = render(<Splide options={options}><SplideSlide><img src={mockData.images[0].picture} alt={mockData.name} /></SplideSlide><SplideSlide><img src={mockData.images[1].picture} alt={mockData.name} /></SplideSlide></Splide>);
+      container = component.container;
+      unmount = component.unmount;
+    });
 
-  it('should render Image Slider', () => {
-    const options = {
-      perPage: 1,
-      perMove: 1,
-      rewind: true,
-      fixedWidth: '350px',
-      fixedHeight: '250px',
-      cover: 'true',
-      pagination: true,
-    };
-    // eslint-disable-next-line max-len
-    render(<Splide options={options}><SplideSlide><img src={mockData.images[0].picture} alt={mockData.name} /></SplideSlide></Splide>);
+    afterEach(() => {
+      unmount();
+    });
+
+    it('should render Image Slider', () => {
+      expect(container.firstChild).toBeInTheDocument();
+    });
+
+    it('should switch slides when arrow button clicked', () => {
+      const button = container.querySelector('.splide__arrow--next');
+      const slides = container.querySelector('.splide__list');
+      expect(slides.firstChild.classList.contains('is-active')).toBe(true);
+      fireEvent.click(button);
+      expect(slides.firstChild.classList.contains('is-active')).toBe(false);
+      expect(slides.firstChild.classList.contains('is-prev')).toBe(true);
+    });
   });
 });
