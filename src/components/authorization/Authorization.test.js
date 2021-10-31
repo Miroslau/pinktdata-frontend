5af62c47e5378df1a30fe9fea6d29d1fa749f8a3
Miroslau <mira2408@mail.ru>
2021-11-01 02:36:03 +0300

    feat: add tests

--- src/components/authorization/Authorization.test.js
+++ src/components/authorization/Authorization.test.js
@@ -4,7 +4,6 @@ import {
 } from '@testing-library/react';
 import Authorization from './Authorization';
 
-// eslint-disable-next-line no-undef
 describe('user registration', () => {
   const isSign = false;
   const openForm = () => true;
@@ -19,12 +18,9 @@ describe('user registration', () => {
     submit: null,
   };
 
-  // eslint-disable-next-line no-unused-vars
   let container;
-  // eslint-disable-next-line no-unused-vars
   let unmount;
 
-  // eslint-disable-next-line no-undef
   beforeEach(() => {
     // eslint-disable-next-line max-len,react/react-in-jsx-scope
     const { container: currentContainer, unmount: currentUnmount } = render(<Authorization openForm={openForm} isSignIn={isSign} submitForm={sendForm} />);
@@ -44,249 +40,176 @@ describe('user registration', () => {
     elementsAuthorization.submit = currentContainer.querySelector('.MuiButton-root');
   });
 
-  // eslint-disable-next-line no-undef
   afterEach(() => {
     unmount();
   });
 
-  // eslint-disable-next-line no-undef
   it('renders component authorization', () => {
-    // eslint-disable-next-line no-shadow,max-len,react/react-in-jsx-scope
-    const { container } = render(<Authorization openForm={openForm} isSignIn={isSign} submitForm={sendForm} />);
-    // eslint-disable-next-line no-undef
+    render(<Authorization openForm={openForm} isSignIn={isSign} submitForm={sendForm} />);
     expect(container.firstChild).toBeInTheDocument();
   });
 
-  // eslint-disable-next-line no-undef
   it('has correct input First name', async () => {
-    // eslint-disable-next-line react/react-in-jsx-scope,max-len
     fireEvent.change(elementsAuthorization.inputName, { target: { value: 'Misha' } });
     fireEvent.blur(elementsAuthorization.inputName);
     fireEvent.click(elementsAuthorization.submit);
     const result = await findByDisplayValue(container, 'Misha');
-    // eslint-disable-next-line no-undef
     expect(result).toBeInTheDocument();
   });
 
-  // eslint-disable-next-line no-undef
   it('has First name must be of length 5 to 32', async () => {
     fireEvent.change(elementsAuthorization.inputName, { target: { value: 'Misk' } });
     fireEvent.blur(elementsAuthorization.inputName);
     fireEvent.click(elementsAuthorization.submit);
     const error = await findByText(container, 'Must be of length 5 to 32');
-    // eslint-disable-next-line no-undef
     expect(error).toBeInTheDocument();
   });
 
-  // eslint-disable-next-line no-undef
   it('has First name must be required', async () => {
     fireEvent.change(elementsAuthorization.inputName, { target: { value: '' } });
     fireEvent.blur(elementsAuthorization.inputName);
     fireEvent.click(elementsAuthorization.submit);
     const error = await findByText(container, 'First Name required');
-    // eslint-disable-next-line no-undef
     expect(error).toBeInTheDocument();
   });
 
-  // eslint-disable-next-line no-undef
   it('has correct input Last name', async () => {
-    // eslint-disable-next-line react/react-in-jsx-scope,max-len
     fireEvent.change(elementsAuthorization.inputLastName, { target: { value: 'Pusick' } });
     fireEvent.blur(elementsAuthorization.inputLastName);
     fireEvent.click(elementsAuthorization.submit);
     const result = await findByDisplayValue(container, 'Pusick');
-    // eslint-disable-next-line no-undef
     expect(result).toBeInTheDocument();
   });
 
-  // eslint-disable-next-line no-undef
   it('has Last name must be of length 5 to 32', async () => {
     fireEvent.change(elementsAuthorization.inputLastName, { target: { value: 'Misk' } });
     fireEvent.blur(elementsAuthorization.inputLastName);
     fireEvent.click(elementsAuthorization.submit);
     const error = await findByText(container, 'Must be of length 5 to 32');
-    // eslint-disable-next-line no-undef
     expect(error).toBeInTheDocument();
   });
 
-  // eslint-disable-next-line no-undef
   it('has Last name must be required', async () => {
     fireEvent.change(elementsAuthorization.inputLastName, { target: { value: '' } });
     fireEvent.blur(elementsAuthorization.inputLastName);
     fireEvent.click(elementsAuthorization.submit);
     const error = await findByText(container, 'First Name required');
-    // eslint-disable-next-line no-undef
     expect(error).toBeInTheDocument();
   });
 
-  // eslint-disable-next-line no-undef
   it('has correct Last name  must including only alphabetical characters', async () => {
     fireEvent.change(elementsAuthorization.inputLastName, { target: { value: 'Test123' } });
     fireEvent.blur(elementsAuthorization.inputLastName);
     fireEvent.click(elementsAuthorization.submit);
     const error = await findByText(container, 'Enter a valid name');
-    // eslint-disable-next-line no-undef
     expect(error).toBeInTheDocument();
   });
 
-  // eslint-disable-next-line no-undef
   it('has correct input Email', async () => {
-    // eslint-disable-next-line react/react-in-jsx-scope,max-len
     fireEvent.change(elementsAuthorization.inputEmail, { target: { value: 'mysite@ourearth.com' } });
     fireEvent.blur(elementsAuthorization.inputEmail);
     fireEvent.click(elementsAuthorization.submit);
-    // eslint-disable-next-line no-undef
     const result = await findByDisplayValue(container, 'mysite@ourearth.com');
-    // eslint-disable-next-line no-undef
     expect(result).toBeInTheDocument();
   });
 
-  // eslint-disable-next-line no-undef
   it('has correct Email must be valid email', async () => {
-    // eslint-disable-next-line react/react-in-jsx-scope,max-len
     fireEvent.change(elementsAuthorization.inputEmail, { target: { value: '.email@example.com' } });
     fireEvent.blur(elementsAuthorization.inputEmail);
     fireEvent.click(elementsAuthorization.submit);
-    // eslint-disable-next-line no-undef
     const error = await findByText(container, 'Email address is invalid');
-    // eslint-disable-next-line no-undef
     expect(error).toBeInTheDocument();
   });
 
-  // eslint-disable-next-line no-undef
   it('has correct Email must be required', async () => {
-    // eslint-disable-next-line react/react-in-jsx-scope,max-len
     fireEvent.change(elementsAuthorization.inputEmail, { target: { value: '' } });
     fireEvent.blur(elementsAuthorization.inputEmail);
     fireEvent.click(elementsAuthorization.submit);
-    // eslint-disable-next-line no-undef
     const error = await findByText(container, 'Email required');
-    // eslint-disable-next-line no-undef
     expect(error).toBeInTheDocument();
   });
 
-  // eslint-disable-next-line no-undef
   it('has correct input Password', async () => {
-    // eslint-disable-next-line react/react-in-jsx-scope,max-len
     fireEvent.change(elementsAuthorization.inputPassword, { target: { value: 'Mira$1234' } });
     fireEvent.blur(elementsAuthorization.inputPassword);
     fireEvent.click(elementsAuthorization.submit);
-    // eslint-disable-next-line no-undef
     const result = await findByDisplayValue(container, 'Mira$1234');
-    // eslint-disable-next-line no-undef
     expect(result).toBeInTheDocument();
   });
 
-  // eslint-disable-next-line no-undef
   it('has correct Password must be required', async () => {
-    // eslint-disable-next-line react/react-in-jsx-scope,max-len
     fireEvent.change(elementsAuthorization.inputPassword, { target: { value: '' } });
     fireEvent.blur(elementsAuthorization.inputPassword);
     fireEvent.click(elementsAuthorization.submit);
-    // eslint-disable-next-line no-undef
     const error = await findByText(container, 'Password required');
-    // eslint-disable-next-line no-undef
     expect(error).toBeInTheDocument();
   });
 
-  // eslint-disable-next-line no-undef
   it('has correct Password must contain mix of letters and numbers', async () => {
-    // eslint-disable-next-line react/react-in-jsx-scope,max-len
     fireEvent.change(elementsAuthorization.inputPassword, { target: { value: 'Mira$' } });
     fireEvent.blur(elementsAuthorization.inputPassword);
     fireEvent.click(elementsAuthorization.submit);
-    // eslint-disable-next-line no-undef
     const error = await findByText(container, 'Must contain mix of letters and numbers');
-    // eslint-disable-next-line no-undef
     expect(error).toBeInTheDocument();
   });
 
-  // eslint-disable-next-line no-undef
   it('has correct Password must contain at least 1 special character', async () => {
-    // eslint-disable-next-line react/react-in-jsx-scope,max-len
     fireEvent.change(elementsAuthorization.inputPassword, { target: { value: 'Mira123' } });
     fireEvent.blur(elementsAuthorization.inputPassword);
     fireEvent.click(elementsAuthorization.submit);
-    // eslint-disable-next-line no-undef
     const error = await findByText(container, 'Must contain at least 1 special character');
-    // eslint-disable-next-line no-undef
     expect(error).toBeInTheDocument();
   });
 
-  // eslint-disable-next-line no-undef
   it('has correct Password must contain at least 1 lowercase letter and 1 uppercase letter', async () => {
-    // eslint-disable-next-line react/react-in-jsx-scope,max-len
     fireEvent.change(elementsAuthorization.inputPassword, { target: { value: 'MIRA123' } });
     fireEvent.blur(elementsAuthorization.inputPassword);
     fireEvent.click(elementsAuthorization.submit);
-    // eslint-disable-next-line no-undef
     const error = await findByText(container, '1 lowercase and 1 uppercase letter');
-    // eslint-disable-next-line no-undef
     expect(error).toBeInTheDocument();
   });
 
-  // eslint-disable-next-line no-undef
   it('has correct Password must contain at least 1 lowercase letter and 1 uppercase letter', async () => {
-    // eslint-disable-next-line react/react-in-jsx-scope,max-len
     fireEvent.change(elementsAuthorization.inputPassword, { target: { value: 'mira123' } });
     fireEvent.blur(elementsAuthorization.inputPassword);
     fireEvent.click(elementsAuthorization.submit);
-    // eslint-disable-next-line no-undef
     const error = await findByText(container, '1 lowercase and 1 uppercase letter');
-    // eslint-disable-next-line no-undef
     expect(error).toBeInTheDocument();
   });
 
-  // eslint-disable-next-line no-undef
   it('has correct Password must be of length 7 to 12', async () => {
-    // eslint-disable-next-line react/react-in-jsx-scope,max-len
     fireEvent.change(elementsAuthorization.inputPassword, { target: { value: 'Mi$123' } });
     fireEvent.blur(elementsAuthorization.inputPassword);
     fireEvent.click(elementsAuthorization.submit);
-    // eslint-disable-next-line no-undef
     const error = await findByText(container, 'must be of length 7 to 12');
-    // eslint-disable-next-line no-undef
     expect(error).toBeInTheDocument();
   });
 
-  // eslint-disable-next-line no-undef
   it('has correct Password must be of length 7 to 12', async () => {
-    // eslint-disable-next-line react/react-in-jsx-scope,max-len
     fireEvent.change(elementsAuthorization.inputPassword, { target: { value: 'Miroslav$1234t' } });
     fireEvent.blur(elementsAuthorization.inputPassword);
     fireEvent.click(elementsAuthorization.submit);
-    // eslint-disable-next-line no-undef
     const error = await findByText(container, 'must be of length 7 to 12');
-    // eslint-disable-next-line no-undef
     expect(error).toBeInTheDocument();
   });
 
-  // eslint-disable-next-line no-undef
   it('has correct input Repeat password', async () => {
-    // eslint-disable-next-line react/react-in-jsx-scope,max-len
     fireEvent.change(elementsAuthorization.repeatPassword, { target: { value: 'Miroslav$1234t' } });
     fireEvent.blur(elementsAuthorization.repeatPassword);
     fireEvent.click(elementsAuthorization.submit);
-    // eslint-disable-next-line no-undef
     const result = await findByDisplayValue(container, 'Miroslav$1234t');
-    // eslint-disable-next-line no-undef
     expect(result).toBeInTheDocument();
   });
 
-  // eslint-disable-next-line no-undef
   it('has correct Repeat password must be required', async () => {
-    // eslint-disable-next-line react/react-in-jsx-scope,max-len
     fireEvent.change(elementsAuthorization.repeatPassword, { target: { value: '' } });
     fireEvent.blur(elementsAuthorization.repeatPassword);
     fireEvent.click(elementsAuthorization.submit);
-    // eslint-disable-next-line no-undef
     const error = await findByText(container, 'Repeat Password required');
-    // eslint-disable-next-line no-undef
     expect(error).toBeInTheDocument();
   });
 });
 
-// eslint-disable-next-line no-undef
 describe('user login', () => {
   const isSign = true;
   const openForm = () => true;
@@ -298,14 +221,11 @@ describe('user login', () => {
     submit: null,
   };
 
-  // eslint-disable-next-line no-unused-vars
   let container;
-  // eslint-disable-next-line no-unused-vars
   let unmount;
 
-  // eslint-disable-next-line no-undef
   beforeEach(() => {
-    // eslint-disable-next-line max-len,react/react-in-jsx-scope
+    // eslint-disable-next-line max-len
     const { container: currentContainer, unmount: currentUnmount } = render(<Authorization openForm={openForm} isSignIn={isSign} submitForm={sendForm} />);
     container = currentContainer;
     unmount = currentUnmount;
@@ -317,56 +237,40 @@ describe('user login', () => {
     elementsAuthorization.submit = currentContainer.querySelector('.MuiButton-root');
   });
 
-  // eslint-disable-next-line no-undef
   afterEach(() => {
     unmount();
   });
 
-  // eslint-disable-next-line no-undef
   it('has correct input Email', async () => {
-    // eslint-disable-next-line react/react-in-jsx-scope,max-len
     fireEvent.change(elementsAuthorization.inputEmail, { target: { value: 'mysite@ourearth.com' } });
     fireEvent.blur(elementsAuthorization.inputEmail);
     fireEvent.click(elementsAuthorization.submit);
-    // eslint-disable-next-line no-undef
     const result = await findByDisplayValue(container, 'mysite@ourearth.com');
-    // eslint-disable-next-line no-undef
     expect(result).toBeInTheDocument();
   });
 
   // eslint-disable-next-line no-undef
   it('has correct Email must be required', async () => {
-    // eslint-disable-next-line react/react-in-jsx-scope,max-len
     fireEvent.change(elementsAuthorization.inputEmail, { target: { value: '' } });
     fireEvent.blur(elementsAuthorization.inputEmail);
     fireEvent.click(elementsAuthorization.submit);
-    // eslint-disable-next-line no-undef
     const error = await findByText(container, 'Email required');
-    // eslint-disable-next-line no-undef
     expect(error).toBeInTheDocument();
   });
 
-  // eslint-disable-next-line no-undef
   it('has correct input Password', async () => {
-    // eslint-disable-next-line react/react-in-jsx-scope,max-len
     fireEvent.change(elementsAuthorization.inputPassword, { target: { value: 'Mira$1234' } });
     fireEvent.blur(elementsAuthorization.inputPassword);
     fireEvent.click(elementsAuthorization.submit);
-    // eslint-disable-next-line no-undef
     const result = await findByDisplayValue(container, 'Mira$1234');
-    // eslint-disable-next-line no-undef
     expect(result).toBeInTheDocument();
   });
 
-  // eslint-disable-next-line no-undef
   it('has correct Password must be required', async () => {
-    // eslint-disable-next-line react/react-in-jsx-scope,max-len
     fireEvent.change(elementsAuthorization.inputPassword, { target: { value: '' } });
     fireEvent.blur(elementsAuthorization.inputPassword);
     fireEvent.click(elementsAuthorization.submit);
-    // eslint-disable-next-line no-undef
     const error = await findByText(container, 'Password required');
-    // eslint-disable-next-line no-undef
     expect(error).toBeInTheDocument();
   });
 });
--- src/components/map-page/tabs/apartment-filters/ApartmentFilters.test.js
+++ src/components/map-page/tabs/apartment-filters/ApartmentFilters.test.js
@@ -7,9 +7,7 @@ import ApartmentFilters from './ApartmentFilters';
 describe('Apartment filter component', () => {
   const apartmentFilter = () => {};
 
-  // eslint-disable-next-line no-unused-vars
   let container;
-  // eslint-disable-next-line no-unused-vars
   let unmount;
 
   beforeEach(() => {
@@ -27,26 +25,105 @@ describe('Apartment filter component', () => {
     render(<ApartmentFilters apartmentFilter={apartmentFilter} />);
   });
 
-  it('has open input when click on label', async () => {
-    render(<ApartmentFilters apartmentFilter={apartmentFilter} />);
+  it('has open input when click on right label', async () => {
     const textLabel = await screen.findByText('Max price: 1000+');
-
     fireEvent.click(textLabel);
-
     const result = await screen.getByText('1000');
-
     expect(result).toBeInTheDocument();
   });
 
-  it('has close input when change slider', async () => {
-    const textLabel = await screen.findByText('Max price: 1000+');
-
+  it('has open input when click on left label', async () => {
+    const textLabel = await screen.findByText('Min price: 10');
     fireEvent.click(textLabel);
+    const result = await screen.getByText('10');
+    expect(result).toBeInTheDocument();
+  });
 
+  it('has close inputs when change slider', async () => {
+    const textLabel = await screen.findByText('Max price: 1000+');
+    fireEvent.click(textLabel);
     const slide = await container.querySelectorAll('input')[1];
-    console.log(slide);
-    // fireEvent.change(slide, { target: { value: '500' } });
-    // const result = await findByText('500');
-    // expect(result).toBeInTheDocument();
+    fireEvent.change(slide, { target: { value: '500' } });
+    fireEvent.blur(slide);
+    const result = await findByText(container, '500');
+    expect(result).toBeInTheDocument();
+  });
+
+  it('has close right input when click on left label', async () => {
+    const textLabel = await screen.findByText('Max price: 1000+');
+    fireEvent.click(textLabel);
+    const rightInput = await container.querySelectorAll('input')[0];
+    fireEvent.change(rightInput, { target: { value: '500' } });
+    fireEvent.blur(rightInput);
+    const leftLabel = await screen.findByText('Min price: 10');
+    fireEvent.click(leftLabel);
+    const result = await findByText(container, '500');
+    expect(result).toBeInTheDocument();
+  });
+
+  it('has close left input when click on right label', async () => {
+    const textLabel = await screen.findByText('Min price: 10');
+    fireEvent.click(textLabel);
+    const rightInput = await container.querySelectorAll('input')[0];
+    fireEvent.change(rightInput, { target: { value: '500' } });
+    fireEvent.blur(rightInput);
+    const leftLabel = await screen.findByText('Max price: 1000+');
+    fireEvent.click(leftLabel);
+    const result = await findByText(container, '500');
+    expect(result).toBeInTheDocument();
+  });
+
+  it('has change count bedrooms when click button plus', async () => {
+    const buttonPlus = await container.querySelectorAll('.MuiButton-root')[1];
+    fireEvent.click(buttonPlus);
+    const result = await findByText(container, '1');
+    expect(result).toBeInTheDocument();
+  });
+
+  it('has change count bedrooms when click button minus', async () => {
+    const buttonPlus = await container.querySelectorAll('.MuiButton-root')[1];
+    fireEvent.click(buttonPlus);
+    fireEvent.click(buttonPlus);
+    const buttonMinus = await container.querySelectorAll('.MuiButton-root')[0];
+    fireEvent.click(buttonMinus);
+    const result = await findByText(container, '1');
+    expect(result).toBeInTheDocument();
+  });
+
+  it('has not change count bedrooms when click disabled button minus', async () => {
+    const buttonMinus = await container.querySelectorAll('.MuiButton-root')[0];
+    fireEvent.click(buttonMinus);
+    const result = await findByText(container, '0');
+    expect(result).toBeInTheDocument();
+  });
+
+  it('has not change count bedrooms when click disabled button plus', async () => {
+    const buttonPlus = await container.querySelectorAll('.MuiButton-root')[1];
+    const countClick = [...Array(7)];
+    countClick.forEach(() => fireEvent.click(buttonPlus));
+    const result = await findByText(container, '5');
+    expect(result).toBeInTheDocument();
+  });
+
+  it('has clear settings price range when click button clear', async () => {
+    const slideRight = await container.querySelectorAll('input')[1];
+    fireEvent.change(slideRight, { target: { value: '500' } });
+    fireEvent.blur(slideRight);
+    const slideLeft = await container.querySelectorAll('input')[0];
+    fireEvent.change(slideLeft, { target: { value: '100' } });
+    fireEvent.blur(slideLeft);
+    const clearButton = await findByText(container, 'Clear');
+    fireEvent.click(clearButton);
+    const result = await findByText(container, 'Max price: 1000+');
+    expect(result).toBeInTheDocument();
+  });
+
+  it('has clear settings count bedrooms when click button clear', async () => {
+    const buttonPlus = await container.querySelectorAll('.MuiButton-root')[1];
+    fireEvent.click(buttonPlus);
+    const clearButton = await findByText(container, 'Clear');
+    fireEvent.click(clearButton);
+    const result = await findByText(container, '0');
+    expect(result).toBeInTheDocument();
   });
 });
