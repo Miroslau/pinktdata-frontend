8ab391fbfcc72de5639ba687f23562a4a4d37382
Dianakove32 <dianakove32@gmail.com>
2021-11-01 19:05:12 +0300

    fix lint error

--- src/api/main-search/LocationApi.test.js
+++ src/api/main-search/LocationApi.test.js
@@ -6,11 +6,10 @@ import { BrowserRouter } from 'react-router-dom';
 import LocationAPI from './LocationAPI';
 import MainSearch from '../../components/section-components/main-screen/main-search/MainSearch';
 
+jest.mock('./LocationAPI.js');
 jest.mock('../../utils/doWithUserDelay.js', () => ({
   doWithUserDelay: (callback) => callback(),
 }));
-
-jest.mock('./LocationAPI.js');
 
 describe('LocationAPI function', () => {
   test('LocationAPI should have been called', async () => {
