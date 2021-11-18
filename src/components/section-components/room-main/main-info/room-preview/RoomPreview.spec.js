057487f130ea5898d88c75ce9e422b2ca0d65173
Finsoy <vadimka.cry.00@gmail.com>
2021-11-18 12:35:38 +0300

    fix: fix and add new test

--- src/components/section-components/room-main/main-info/room-preview/RoomPreview.spec.js
+++ src/components/section-components/room-main/main-info/room-preview/RoomPreview.spec.js
@@ -1,15 +1,49 @@
 import { render, screen } from '@testing-library/react';
 import React from 'react';
+import { Provider } from 'react-redux';
+import { combineReducers, configureStore, createSlice } from '@reduxjs/toolkit';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import { roomContext } from '../../../../../store/context/roomContext';
 import RoomPreview from './RoomPreview';
 import mockDataForPreviewPage from '../../../../../mocks/mocks-constants/mockDataForPreviewPage';
 
 describe('RoomPreview component', () => {
+  const apartmentSlice = createSlice({
+    name: 'apartment',
+    initialState: {
+      startDate: 'Thu Nov 18 2021 12:26:52 GMT+0300 (Москва, стандартное время)',
+      endDate: 'Fri Nov 19 2021 12:26:52 GMT+0300 (Москва, стандартное время)',
+    },
+  });
+
+  const apartmentReducer = apartmentSlice.reducer;
+
+  const persistConfig = {
+    key: 'root',
+    storage,
+  };
+
+  const persistedReducer = persistReducer(persistConfig, apartmentReducer);
+
+  const reducers = combineReducers({
+    apartment: persistedReducer,
+  });
+
+  const rootReducer = (state, action) => reducers(state, action);
+
+  const store = configureStore({
+    reducer: rootReducer,
+  });
+
   test('should have render images', () => {
     render((
-      <roomContext.Provider value={mockDataForPreviewPage}>
-        <RoomPreview />
-      </roomContext.Provider>
+      <Provider store={store}>
+        <roomContext.Provider value={mockDataForPreviewPage}>
+          <RoomPreview />
+        </roomContext.Provider>
+      </Provider>
+
     ));
 
     expect(screen.getAllByAltText(/slider/i)).toBeTruthy();
@@ -17,21 +51,25 @@ describe('RoomPreview component', () => {
 
   test('should have render main image', () => {
     render((
-      <roomContext.Provider value={mockDataForPreviewPage}>
-        <RoomPreview />
-      </roomContext.Provider>
+      <Provider store={store}>
+        <roomContext.Provider value={mockDataForPreviewPage}>
+          <RoomPreview />
+        </roomContext.Provider>
+      </Provider>
     ));
 
     expect(screen.getByAltText(/room-preview/i)).toBeTruthy();
   });
 
-  test('should have render room info paragraph', () => {
+  test('should have render room total price information', () => {
     render((
-      <roomContext.Provider value={mockDataForPreviewPage}>
-        <RoomPreview />
-      </roomContext.Provider>
+      <Provider store={store}>
+        <roomContext.Provider value={mockDataForPreviewPage}>
+          <RoomPreview />
+        </roomContext.Provider>
+      </Provider>
     ));
 
-    expect(screen.getByText(/Room info/i)).toBeTruthy();
+    expect(screen.getByText(/Total price/i)).toBeTruthy();
   });
 });
