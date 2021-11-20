e90654178d20a1729d29f188f8f53b7bf08239e2
Kath Veremeychik <kateiiverem@gmail.com>
2021-11-20 17:26:55 +0300

    fix: change style of modal window

--- src/components/profile-page/NewRoom/NewRoom.jsx
+++ src/components/profile-page/NewRoom/NewRoom.jsx
@@ -1,80 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FileUploader } from 'react-drag-drop-files';
 import PropTypes from 'prop-types';
-import { styled } from '@mui/material/styles';
+
+// import { styled } from '@mui/material/styles';
 import MenuItem from '@mui/material/MenuItem';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
-import ButtonMui from '../../ui-components/button-mui/ButtonMui';
 import useStyles from '../Profile.style';
 import './NewRoom.modify.css';
-import { newRoomAmountField, newRoomTextField } from '../../../constants/newRoom/newRoom';
+
+import ButtonMui from '../../ui-components/button-mui/ButtonMui';
 import TextFieldMui from '../../ui-components/text-field-mui/TextFieldMui';
+// import TypographyMui from '../../ui-components/typography-mui/TypographyMui';
+import { newRoomAmountField, newRoomTextField } from '../../../constants/newRoom/newRoom';
+import { fileTypes } from '../../../constants/newRoom/fileTypes';
+import { currencies } from '../../../constants/newRoom/currency';
 import useForm from '../../../hooks/useForm';
 import { validateErrors } from '../../../mixins/validateErrors';
-import TypographyMui from '../../ui-components/typography-mui/TypographyMui';
-import { currencies } from '../../../constants/newRoom/currency';
 
-const Input = styled('input')({
-  display: 'none',
-});
+// const Input = styled('input')({
+//   display: 'none',
+// });
 
-const NewRoom = function ({ active, setActive }) {
+const NewRoom = function ({ setActive }) {
   const {
     handleChange, errors,
   } = useForm(
     validateErrors,
   );
   const classes = useStyles();
-  const [currency, setCurrency] = React.useState('USD');
+  const [currency, setCurrency] = useState('USD');
+  const [file, setFile] = useState(null);
 
   const currencyHandler = (event) => {
     setCurrency(event.target.value);
   };
 
+  const fileHandler = () => {
+    setFile(file);
+  };
+
   return (
-    // eslint-disable-next-line max-len
-    // eslint-disable-next-line jsx-a11y/no-static-element-interactions,jsx-a11y/click-events-have-key-events
-    <div
-      className={active ? 'modal active' : 'modal'}
-      onClick={() => setActive(false)}
-    >
-      {/* eslint-disable-next-line max-len */}
-      {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions */}
-      <div
-        className={active ? 'modal__content active' : 'modal__content'}
-        onClick={(event) => event.stopPropagation()}
-      >
-        <Typography
-          className={classes.modalTitle}
-          variant="h5"
-          gutterBottom
-        >
-          Add New Room
-        </Typography>
-        <Card>
-          <CardContent>
-            <form
-              className={classes.modalForm}
-              noValidate
-              autoComplete="off"
-            >
-              <Grid container spacing={2}>
-                <Grid item xs={6} md={6}>
-                  <Box
-                    component="form"
-                    sx={{
-                      '& .MuiTextField-root': { m: 1, width: '60ch' },
-                    }}
-                    noValidate
-                    autoComplete="off"
-                  >
-                    <div>
-                      {
+    <div>
+      <Card>
+        <CardContent>
+          <form>
+            <Grid container spacing={2}>
+              <Grid item xs={6} md={8}>
+                <Box>
+                  <div>
+                    {
                         newRoomTextField.map((field) => (
                           <TextFieldMui
                             variant="outlined"
@@ -87,38 +66,33 @@ const NewRoom = function ({ active, setActive }) {
                             label={field.title}
                             placeholder={field.placeholder}
                             inputText={handleChange}
+                            margin="dense"
                           />
                         ))
                       }
-                    </div>
-                  </Box>
-                </Grid>
-                <Grid item xs={4} md={4} className={classes.textField}>
-                  <Box
-                    component="form"
-                    sx={{
-                      '& .MuiTextField-root': { m: 1, width: '38ch' },
-                    }}
-                    noValidate
-                    autoComplete="off"
-                  >
-                    <div>
-                      <TextField
-                        id="outlined-select-currency"
-                        select
-                        label="select"
-                        value={currency}
-                        onChange={currencyHandler}
-                        helperText="Please select your currency"
-                        size="small"
-                      >
-                        {currencies.map((option) => (
-                          <MenuItem key={option.value} value={option.value}>
-                            {option.label}
-                          </MenuItem>
-                        ))}
-                      </TextField>
-                      {
+                  </div>
+                </Box>
+              </Grid>
+              <Grid item xs={6} md={4} className={classes.textField}>
+                <Box>
+                  <div>
+                    <TextField
+                      id="outlined-select-currency"
+                      select
+                      label="select"
+                      value={currency}
+                      onChange={currencyHandler}
+                      helperText="Please select your currency"
+                      size="small"
+                      margin="dense"
+                    >
+                      {currencies.map((option) => (
+                        <MenuItem key={option.value} value={option.value}>
+                          {option.label}
+                        </MenuItem>
+                      ))}
+                    </TextField>
+                    {
                           newRoomAmountField.map((field) => (
                             <TextFieldMui
                               variant="outlined"
@@ -132,43 +106,35 @@ const NewRoom = function ({ active, setActive }) {
                               placeholder={field.placeholder}
                               inputText={handleChange}
                               size="small"
+                              margin="dense"
                             />
                           ))
                         }
-                    </div>
-                  </Box>
-                </Grid>
-                <Grid item xs={2} md={2}>
-                  <div className={classes.uploadBtn}>
-                    <TypographyMui variant="h6" text="Upload your files here:" />
-                    {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-                    <label htmlFor="contained-button-file">
-                      <Input accept="image/*" id="contained-button-file" multiple type="file" />
-                      <ButtonMui variant="contained" component="span">
-                        Browse
-                      </ButtonMui>
-                    </label>
                   </div>
-                </Grid>
+                </Box>
               </Grid>
-            </form>
-          </CardContent>
-          <CardActions className={classes.buttonForm}>
-            <ButtonMui title="save" size="large" clickButton={() => setActive(false)} />
-          </CardActions>
-        </Card>
-      </div>
+            </Grid>
+            <FileUploader
+              handleChange={fileHandler}
+              name="file"
+              types={fileTypes}
+              classes="file-upload"
+            />
+          </form>
+        </CardContent>
+        <CardActions className={classes.buttonForm}>
+          <ButtonMui title="save" size="large" clickButton={() => setActive(false)} />
+        </CardActions>
+      </Card>
     </div>
   );
 };
 
 NewRoom.defaultProps = {
-  active: false,
-  setActive: null,
+  setActive: true,
 };
 
 NewRoom.propTypes = {
-  active: PropTypes.bool,
   setActive: PropTypes.func,
 };
 
--- src/pages/profile-page/ProfilePage.jsx
+++ src/pages/profile-page/ProfilePage.jsx
@@ -1,28 +1,49 @@
 import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+
 import Typography from '@mui/material/Typography';
-import useStyles from '../../components/profile-page/Profile.style';
+import useStyles from '../../style/style';
+
 import History from '../../components/profile-page/History';
 import FutureVisits from '../../components/profile-page/FutureVisits';
 import Rent from '../../components/profile-page/Rent';
 import ButtonMui from '../../components/ui-components/button-mui/ButtonMui';
 import NewRoom from '../../components/profile-page/NewRoom/NewRoom';
+import ModalWindowMui from '../../components/ui-components/modal-window-mui/ModalWindowMui';
+import { userSelector } from '../../store/slice/userSlice';
 
 const ProfilePage = function () {
   const classes = useStyles();
-  const [isModalActive, setIsModalActive] = useState(false);
+  const [isActiveModal, setModalActive] = useState(false);
+
+  const {
+    isError, errorMessage,
+  } = useSelector(
+    userSelector,
+  );
 
   return (
-    <div className={classes.wrapper}>
+    <div className={classes.modalWrapper}>
       <div className={classes.header}>
         <Typography variant="body1" gutterBottom className={classes.title}>
           Dashboard
         </Typography>
-        <ButtonMui title="Add new room" variant="outlined" onClick={setIsModalActive} />
+        <ButtonMui title="Add new room" variant="outlined" onClick={() => setModalActive(true)} />
       </div>
       <History />
       <FutureVisits />
       <Rent />
-      <NewRoom active={isModalActive} setActive={setIsModalActive} />
+      <ModalWindowMui
+        clickButton={() => setModalActive(false)}
+        title="Add New Room"
+        isActiveModal={isActiveModal}
+        sx={classes.dialog}
+      >
+        {
+          isError && <div className="navigation-landing-error">{errorMessage}</div>
+        }
+        <NewRoom setActive={setModalActive} />
+      </ModalWindowMui>
     </div>
   );
 };
